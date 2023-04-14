8cb25809ba3e4878ef8ded2bd0f6c9e6d65c2ac4
taskooh <taskooh@github.com>
2023-04-15 00:54:06 +0900

    upload voice

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import urlJoin from 'url-join';
-import './App.css';
+import React from "react";
+import urlJoin from "url-join";
+import "./App.css";
+import RecordButton from "./RecordButton";
+import { Typography } from "@mui/material";
 
-const apiUrl = "http://127.0.0.1:5000"
+const apiUrl = "http://127.0.0.1:5000";
 
 function App() {
-  const [data, setData] = useState(null);
+  const handleSendWav = async (blob) => {
+    const url = urlJoin(apiUrl, "/api/upload-wav");
+    const formData = new FormData();
+    formData.append("file", blob);
+    const response = await fetch(url, { method: "POST", body: formData });
+    console.log(response);
 
-  useEffect(() => {
-    (async () => {
-      const url = urlJoin(apiUrl, '/api/data')
-      console.log(url)
-      const response = await fetch(urlJoin(apiUrl, '/api/data'));
-      const data = await response.json();
-      setData(data);
-    })()
-  }, []);
+    return response;
+  };
 
   return (
-    <div>
-      {data ? (
-        <div>
-          <p>Name: {data.name}</p>
-          <p>Age: {data.age}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#f5f5f5",
+      }}
+    >
+      <Typography variant="h4" component="h1" sx={{ marginBottom: 2 }}>
+        Recover Your Key
+      </Typography>
+      <RecordButton sendToFlask={handleSendWav} />
     </div>
   );
 }
