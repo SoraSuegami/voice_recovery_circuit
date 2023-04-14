b5b86e68ae8b2d3787cc3b33332fcf45052e38b3
sheagrief <3a.mad1earth4@gmail.com>
2023-04-15 04:21:40 +0900

    Add BCH error correction

---
+++ backend/utils.py
@@ -0,0 +1,47 @@
+import bchlib
+import os
+import random
+
+# bch符号による誤り訂正
+def bch_error_correction(packet):
+    '''
+    BCH符号による誤り訂正
+    Parameters
+    ----------
+    packet : bytearray
+        256ビットのデータをBCHによってエンコードしたもの。256ビットより大きい。
+    '''
+    
+    # create a bch object
+    BCH_POLYNOMIAL = 8219
+    BCH_BITS = 64 #誤り訂正可能なビット数
+    bch = bchlib.BCH(BCH_POLYNOMIAL, BCH_BITS)
+
+    # de-packetize
+    data, ecc = packet[:-bch.ecc_bytes], packet[-bch.ecc_bytes:]
+
+    # correct
+    bitflips = bch.decode_inplace(data, ecc)
+
+    # packetize
+    packet = data + ecc
+
+    return packet
+
+def bitflip(packet):
+        byte_num = random.randint(0, len(packet) - 1)
+        bit_num = random.randint(0, 7)
+        packet[byte_num] ^= (1 << bit_num)
+
+def test_bch(): 
+    data = bytearray(os.urandom(32))    
+    # create a bch object
+    BCH_POLYNOMIAL = 8219
+    BCH_BITS = 64 #誤り訂正可能なビット数
+    bch = bchlib.BCH(BCH_POLYNOMIAL, BCH_BITS)
+
+    ecc = bch.encode(data)
+    packet = data + ecc
+    print(type(packet))
+
+    assert packet == bch_error_correction(packet)
