a473beb7134c03110ba4aa3e6d98f2cd911d09ac
taskooh <taskooh@github.com>
2023-04-15 14:49:36 +0900

    success deploy ENS

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,19 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Get the contract factory
+  const ENSDeployer = await ethers.getContractFactory('ENSDeployer');
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // Deploy the contract
+  console.log('Deploying ENSDeployer...');
+  const ensDeployer = await ENSDeployer.deploy();
+  await ensDeployer.deployed();
+  console.log('ENSDeployer deployed to:', ensDeployer.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
